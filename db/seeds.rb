8680ce2200de7b60191f80bea8a61b00c598c1fb
samuel van de ven <samuel.vandeven@gmail.com>
2022-11-16 12:32:18 +0000

    restaurant model + controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Destroying all restaurants"
+Restaurant.destroy_all
+
+puts "Creating 5 new restaurants"
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: CATEGORY.sample
+  )
+  puts "Created restaurant with id #{restaurant.id}"
+end
+
+
+puts "Finished"
+
+
+
+
+# t.string "name"
+# t.string "address"
+# t.string "phone_number"
+# t.string "category"
